3f9025584a7efc8e9aab5b67f418cce539ad360d
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-08-11 22:26:04 +0530

    py

--- task/practice5.js
+++ task/practice5.js
@@ -16,3 +16,21 @@ function fun1(){
     document.querySelector('#deef').textContent=result=""
 
 }
+function fun(){
+    let input=document.querySelector('#input').value 
+    let result=document.querySelector('#deef')
+
+    if(input==''){
+        prompt("enter the sentence")
+    }
+    else{
+        result.textContent=input
+    }
+
+
+}
+function fun1(){
+    document.querySelector('#input') .textContent =input.value=" "
+    document.querySelector('#deef').textContent=result=""
+
+}
